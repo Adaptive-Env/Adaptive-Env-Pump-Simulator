4a30223f48d504cccf24a59eed89cc216751b8d9
Wiarr3 <wiora.biznes@gmail.com>
2025-10-05 22:09:37 +0200

    nota

--- src/main/java/com/adaptive/environments/pump_simulator/config/KafkaDeviceDataProducerConfig.java
+++ src/main/java/com/adaptive/environments/pump_simulator/config/KafkaDeviceDataProducerConfig.java
@@ -28,5 +28,6 @@ public class KafkaDeviceDataProducerConfig {
 
         SenderOptions<String, DeviceData> options = SenderOptions.create(props);
         return KafkaSender.create(options);
+
     }
 }
